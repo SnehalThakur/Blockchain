487c5edde2d0ea5904772014c92e077c43103282
Snehal Thakur <snehal.hadoop@gmail.com>
2022-12-16 11:25:01 +0530

    Add files via upload

--- Kaarpool-Blockchain/static/js/driverRegister.js
+++ Kaarpool-Blockchain/static/js/driverRegister.js
@@ -1,25 +1,30 @@
 function signupFunction(){
-    var firstName = document.getElementById("firstName").value;
-    var lastName = document.getElementById("lastName").value;
+    var name = document.getElementById("name").value;
     var contactNo = document.getElementById("contactNo").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
+    var licenseNumber = document.getElementById("licenseNumber").value;
+    var licenseValidity = document.getElementById("licenseValidity").value;
     var e = document.getElementById("gender");
     var value = e.value;
+    var v = document.getElementById("vehicle");
+    var vehicleValue = v.value;
     var gender = e.options[e.selectedIndex].text;
-    console.log("firstName: -",firstName)
-    if(firstName == "" || lastName == "" ||contactNo == "" || password == "" || value == 0 || email ==""){
+    var vehicle = v.options[e.selectedIndex].text;
+    console.log("name: -",name)
+    if(name == "" ||contactNo == "" || password == "" || value == 0 || email =="" || vehicleValue == 0 || licenseNumber=="" || licenseValidity==""){
         alert("please fill all fields")
     }
     else{
         let payload = {
-           "firstName":firstName,
-           "lastName":lastName,
+           "name":name,
            "contactNo":contactNo,
            "password":password,
            "gender":gender,
-           "email": email
-
+           "vehicle":vehicle,
+           "email": email,
+           "licenseNumber": licenseNumber,
+           "licenseValidity": licenseValidity
         }
         console.log("payload: - ",payload);
         const userAction = async () => {
--- Kaarpool-Blockchain/static/js/riderRegister.js
+++ Kaarpool-Blockchain/static/js/riderRegister.js
@@ -1,20 +1,18 @@
 function signupFunction(){
-    var firstName = document.getElementById("firstName").value;
-    var lastName = document.getElementById("lastName").value;
+    var name = document.getElementById("name").value;
     var contactNo = document.getElementById("contactNo").value;
     var password = document.getElementById("password").value;
     var email = document.getElementById("email").value;
     var e = document.getElementById("gender");
     var value = e.value;
     var gender = e.options[e.selectedIndex].text;
-    console.log("firstName: -",firstName)
-    if(firstName == "" || lastName == "" ||contactNo == "" || password == "" || value == 0 || email == ""){
+    console.log("name: -",name)
+    if(name == "" ||contactNo == "" || password == "" || value == 0 || email == ""){
         alert("please fill all fields");
     }
     else{
         let payload = {
-           "firstName":firstName,
-           "lastName":lastName,
+           "name":name,
            "contactNo":contactNo,
            "password":password,
            "gender":gender,
